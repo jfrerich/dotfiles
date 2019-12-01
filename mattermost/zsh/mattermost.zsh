d3b45bfebce286b5c75856935b756676ee7d63b7
Jason Frerich <jason.frerich@mattermost.com>
2019-11-30 19:49:42 -0600

    add function for user2sysadmin

--- mattermost/zsh/mattermost.zsh
+++ mattermost/zsh/mattermost.zsh
@@ -28,13 +28,21 @@ mm_user() {
     go run ${mm_dir}/mattermost-server/cmd/mattermost/main.go user create --email $1@example.com --username $1 --password password1
 }
 
+mm_user_actviate() {
+    go run ${mm_dir}/mattermost-server/cmd/mattermost/main.go user $1@example.com 
+}
+
 mm_userbot() {
     go run ${mm_dir}/mattermost-server/cmd/mattermost/main.go user create --email $1@example.com --username $1 --password password1
     go run ${mm_dir}/mattermost-server/cmd/mattermost/main.go user convert $1 --bot
 }
 
+mm_user2sysadmin() {
+    go run ${mm_dir}/mattermost-server/cmd/mattermost/main.go user convert --username $1 --system_admin
+}
+
 mm_usersysadmin() {
-    go run ${mm_dir}/mattermost-server/cmd/mattermost/main.go user create --email $1@example.com --username $1 --password password1 --sysadmin
+    go run ${mm_dir}/mattermost-server/cmd/mattermost/main.go user create --email $1@example.com --username $1 --password password1 --system_admin
 }
 
 mm_bot2user() {
