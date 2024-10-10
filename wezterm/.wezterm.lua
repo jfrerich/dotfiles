9b53e1206ce3e726ea860a2c6a8040263d899085
jfrerich <jasonfrerich@yahoo.com.com>
2024-10-09 19:22:13 -0500

    remove wezterm audible bell

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -5,5 +5,6 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "wsl.exe", "--distribution", "Ubuntu", "--cd", "~" }
 end
 config.color_scheme = "Catppuccin Mocha"
+config.audible_bell = "Disabled"
 
 return config
