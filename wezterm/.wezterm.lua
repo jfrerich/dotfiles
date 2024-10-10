314aece64bb8f6c2f9ad39dec2b81d3e9e64b468
jfrerich <jasonfrerich@yahoo.com.com>
2024-10-09 19:35:33 -0500

    maximize wezterm when open new terminal

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,11 @@
 local wezterm = require("wezterm")
 local config = {}
+local mux = wezterm.mux
+
+wezterm.on("gui-startup", function(cmd)
+	local tab, pane, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "wsl.exe", "--distribution", "Ubuntu", "--cd", "~" }
